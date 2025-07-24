e9ae20d4a4326ee694b796cad05f57a6d9df381c
mlntcandy <me@mlntcandy.com>
2025-07-24 22:04:29 +0300

    works lol

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import "./style.css";
+import { Bime, type BimeComponents } from "./timekeep";
+
+function display(comp: BimeComponents) {
+  const t = document.querySelector(".time")!;
+  const componentEls = {
+    bour: t.querySelector('.digit[data-digit="br"]')!,
+    binute: t.querySelector('.digit[data-digit="bin"]')!,
+    becond: t.querySelector('.digit[data-digit="bec"]')!,
+    sibibecond: t.querySelector('.digit[data-digit="sibibec"]')!,
+  };
+  Object.entries(componentEls).forEach(([k, el]) => {
+    let c = comp[k as keyof BimeComponents];
+    el.querySelectorAll(".binboxes .box").forEach((el, i) => {
+      const set = !!(c & (1 << (3 - i)));
+      el.setAttribute("data-on", `${set}`);
+    });
+    el.querySelector(".hex")!.innerHTML = c.toString(16);
+    el.querySelector(".dec")!.innerHTML = c.toString(10);
+  });
+}
+
+setInterval(() => {
+  display(Bime.fromDate(new Date()).timeAsComponents());
+}, Bime.MS_PER_SIBIBEC_APPROX);
---
+++ src/timekeep.ts
@@ -0,0 +1,36 @@
+export class Bime {
+  // https://discord.com/channels/574720535888396288/1071784467036913664/1397967999394386042
+  static EPOCH = 1753371681447n;
+
+  static NS_PER_SIBIBEC = 1318359375n;
+  static MS_PER_SIBIBEC_APPROX = Number(this.NS_PER_SIBIBEC) / 1_000_000;
+
+  sibibec: bigint;
+
+  private constructor(sibibec: bigint) {
+    this.sibibec = sibibec;
+  }
+  static fromDate(date: Date) {
+    const nsSinceEpoch = (BigInt(date.getTime()) - this.EPOCH) * 1_000_000n;
+    const sibibec = nsSinceEpoch / this.NS_PER_SIBIBEC;
+    return new this(sibibec);
+  }
+  timeAsComponents(): BimeComponents {
+    const t = this.sibibec;
+    return {
+      sibibecond: Number(t & 0xfn),
+      becond: Number((t & 0xf0n) >> 4n),
+      binute: Number((t & 0xf00n) >> 8n),
+      bour: Number((t & 0xf000n) >> 12n),
+      daySinceEpoch: Number((t & 0xf0000n) >> 16n),
+    };
+  }
+}
+
+export type BimeComponents = {
+  sibibecond: number;
+  becond: number;
+  binute: number;
+  bour: number;
+  daySinceEpoch: number;
+};
